4729100c64f21d195e895dbb73fe5252c2cc768a
pijusr <pijusranonis@gmail.com>
2023-04-25 19:39:01 +0300

    Commit V1.3.1

--- Commands/msg_commands.js
+++ Commands/msg_commands.js
@@ -1,15 +1,17 @@
 function As(msg, client){
     if(((msg.content).substring(0,2)).toUpperCase() == "AŠ"
-        && (msg.author.id) != (client.user.id)){
-        obj = (msg.content).substring(2, (msg.content).length);
-        msg.reply(`Labas, ${obj}, aš tėvas!`)
+        && (msg.author.id) != (client.user.id)
+        && !(msg.content).includes("@")
+        && msg.content != 'aš'){
+            obj = (msg.content).substring(2, (msg.content).length);
+            msg.reply(`Labas, ${obj}, aš tėvas!`)
     }
     if(((msg.content).substring(0,2)).toUpperCase() == "AS"
         && (msg.author.id) != (client.user.id)
         && !(msg.content).includes("@")
         && msg.content != 'as'){
-        obj = (msg.content).substring(2, (msg.content).length);
-        msg.reply(`Labas, ${obj}, aš tėvas!`)
+            obj = (msg.content).substring(2, (msg.content).length);
+            msg.reply(`Labas, ${obj}, aš tėvas!`)
     }
 }
 
@@ -18,7 +20,8 @@ function Nerek(msg, client){
         (msg.author.id) != (client.user.id) && 
         (msg.content).substring(0,2) != "XD" &&
         (msg.content).length > 3 &&
-        (msg.content).match(/[a-zA-Z]/g)
+        (msg.content).match(/[a-zA-Z]/g) &&
+        (msg.sontent).substring(0,1) != ':'
         ){
 
         msg.reply(`NERĖK K#RVA!`)
